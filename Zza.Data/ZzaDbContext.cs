95a52c46bce5cd6308724b1ac41ad031c765c3d7
knolegsb <seon2you@hotmail.com>
2015-10-08 00:16:31 -0700

    implemented data and entities

---
+++ Zza.Data/ZzaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace Zza.Data
+{
+    public class ZzaDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+    }
+}
