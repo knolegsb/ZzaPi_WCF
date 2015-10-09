5aef5cd883f47a3571ab2060da29dbb8e3bee2fa
knolegsb <seon2you@hotmail.com>
2015-10-09 00:40:49 -0700

    adding client

---
+++ Zza.Client/ZzaProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zaa.Services;
+using System.ServiceModel;
+using Zza.Client.ZzaServices;
+using System.ServiceModel.Channels;
+using System.Collections.ObjectModel;
+
+namespace Zza.Client
+{
+    class ZzaProxy : ClientBase<IZzaService>, IZzaService
+    {
+        public ZzaProxy() { }
+        public ZzaProxy(string endpointName) : base(endpointName) { }
+        public ZzaProxy(Binding binding, string address) : base(binding, new EndpointAddress(address)) { }
+        public ObservableCollection<Entities.Product> GetProducts()
+        {
+            return Channel.GetProducts();
+        }
+    }
+}
